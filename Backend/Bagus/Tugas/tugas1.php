ce6ec0ba6d46dede4c61b02b987e9ae857fd966f
Bagus Pranowo <bagus.pranowo@students.amikom.ac.id>
2020-04-18 10:46:06 +0700

    Bagus Update

---
+++ Backend/Bagus/Tugas/tugas1.php
@@ -0,0 +1,14 @@
+<?php
+echo "Halo WebDevs19";
+$x = 3;
+$y = 2;
+echo 'Hasil dari x + y = '.($x+$y);
+if ($x>$y)
+{
+    echo 'x lebih besar daripada y';
+}
+else
+{
+    echo 'x lebih kecil daripada y';
+}
+?>

44e1b4ae5efdda2a5315254262ff4bf1b6461d54
Bagus Pranowo <bagus.pranowo@students.amikom.ac.id>
2020-04-23 17:38:06 +0700

    Update kalkulator (2)

--- Backend/Bagus/Tugas/Kalkulator/webdevs-2.php
+++ Backend/Bagus/Tugas/Kalkulator/webdevs-2.php
@@ -6,6 +6,10 @@ if($x>9||$y>9)
 {
     echo "Jangan lebih dari 9 bos, tapi hasilnya " . $z . " kalau penasaran";
 } else {
-    echo $z;
+    echo "hasil ". $x. " x " . $y . " = " . $z;
+    echo "<br>";
+    for($i=1;$i<=$z;$i++){
+        echo $i . " ";
+    }
 }
 ?>

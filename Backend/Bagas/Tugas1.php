f7438e200442998aadcf09f6e9c703539d0e243a
bagas <bagas.nugroho@students.amikom.ac.id>
2020-04-25 23:09:10 +0700

    TugasPertama

---
+++ Backend/Bagas/Tugas1.php
@@ -0,0 +1,29 @@
+<?php
+echo "Hallo WebDevs19<br>"
+?>
+
+<?php
+$txt = " Dari ";
+echo "Hasil" . $txt . "Pertambahan";
+?>
+
+<?php
+$x = 3;
+$y = 2;
+echo "x + y = ".($x+$y);
+?>
+
+<?php
+
+$x = 5;
+$y = 10;
+echo "<br/>";
+if ($x>$y)
+{
+    echo " 10 lebih besar daripada 5 <br> ";
+}
+else
+{
+    echo " 5 lebih kecil daripada 10 <br>";
+}
+?>

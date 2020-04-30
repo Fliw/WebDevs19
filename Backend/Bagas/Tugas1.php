4f258bf55e5fc9a893200774d9a7bb7b39adc1ac
bagas <bagas.nugroho@students.amikom.ac.id>
2020-04-30 19:58:49 +0700

    Tugas Pertama dan Tugas Kalkulator

--- Backend/Bagas/Tugas1.php
+++ Backend/Bagas/Tugas1.php
@@ -1,24 +1,12 @@
 <?php
-echo "Hallo WebDevs19<br>"
-?>
+echo "Hallo WebDevs19<br>";
 
-<?php
-$txt = " Dari ";
-echo "Hasil" . $txt . "Pertambahan";
-?>
-
-<?php
 $x = 3;
 $y = 2;
-echo "x + y = ".($x+$y);
-?>
-
-<?php
-
-$x = 5;
-$y = 10;
+echo 'hasil dari x + y = '.($x+$y);
 echo "<br/>";
-if ($x>$y)
+
+if ($x<$y)
 {
     echo " 10 lebih besar daripada 5 <br> ";
 }

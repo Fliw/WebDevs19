a0019291deffe1a6d674cab78210cf7c4dfd58a3
Bagus Pranowo <bagus.pranowo@students.amikom.ac.id>
2020-05-02 11:26:07 +0700

    tugas frontend & backend w

--- Backend/Bagus/Tugas/Kalkulator tapi banyak opsi/webdevs-3.php
+++ Backend/Bagus/Tugas/Kalkulator tapi banyak opsi/webdevs-3.php
@@ -1,19 +1,20 @@
 <?php
 
-$x = $_GET['angka1'];
-$y = $_GET['angka2'];
-$pilihan = $_GET['pilihan'];
+$data_array = array(
+    'angka1'=>$_GET['angka1'],
+    'angka2'=>$_GET['angka2'],
+    'operasi'=>$_GET['pilihan']
+);
 
-if($pilihan=='tambah'){
-    $z = $x+$y;
-    echo "Hasilnya dari " . $x . " + " . $y . " = " . $z;
-} else if($pilihan=='kurang'){
-    $z = $x-$y;
-    echo "Hasilnya dari " . $x . " - " . $y . " = " . $z;
-} else if($pilihan=='kali'){
-    $z = $x*$y;
-    echo "Hasilnya dari " . $x . " * " . $y . " = " . $z;
-} else if ($pilihan=='bagi'){
-    $z = $x/$y;
-    echo "Hasilnya dari " . $x . " / " . $y . " = " . $z;
+function kalkulasi ($x,$y,$pilihan){
+    if ($pilihan=='tambah'){
+        return $x+$y;
+    } else if($pilihan=='kurang'){
+        return $x-$y;
+    } else if ($pilihan=='kali'){
+        return $x*$y;
+    } else if ($pilihan=='bagi'){
+        return $x/$y;
+    }
 }
+echo "hasilnya adalah " . kalkulasi($data_array['angka1'],$data_array['angka2'],$data_array['operasi']);
